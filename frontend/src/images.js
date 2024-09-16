5a3979aaa9253cff5df98c09577bbb330998deb4
BAMBA-FALL <khadimmbackefall1@gmail.com>
2024-09-16 18:17:47 +0200

    commit ui

---
+++ frontend/src/images.js
@@ -0,0 +1,79 @@
+// src/images.js
+import salade1 from './images/Salades/Salades1.jpg';
+import salade2 from './images/Salades/Salades2.jpg';
+// ...importez toutes les autres images de salades
+
+import burger1 from './images/Burgers/Burgers1.jpg';
+import burger2 from './images/Burgers/Burgers2.jpg';
+// ...importez toutes les autres images de burgers
+
+import pizza1 from './images/Pizzas/Pizzas1.jpg';
+import pizza2 from './images/Pizzas/Pizzas2.jpg';
+// ...importez toutes les autres images de pizzas
+
+import kebab1 from './images/Kebabs/Kebabs1.jpg';
+import kebab2 from './images/Kebabs/Kebabs2.jpg';
+// ...importez toutes les autres images de kebabs
+
+import dessert1 from './images/Desserts/Desserts1.jpg';
+import dessert2 from './images/Desserts/Desserts2.jpg';
+// ...importez toutes les autres images de desserts
+
+import boisson1 from './images/Boissons/Boissons1.jpg'
+import boisson2 from './images/Boissons/Boissons2.jpg'
+// import boisson3 from './images/Boissons/Boissons3'
+// import boisson4 from './images/Boissons/Boissons4'
+// import boisson5 from './images/Boissons/Boissons5'
+// import boisson6 from './images/Boissons/Boissons6'
+// import boisson7 from './images/Boissons/Boissons7'
+// import boisson8 from './images/Boissons/Boissons8'
+// import boisson9 from './images/Boissons/Boissons9'
+// import boisson10 from './images/Boissons/Boissons10'
+
+
+
+
+export const saladeImages = {
+  'Salades1.jpg': salade1,
+  'Salades2.jpg': salade2,
+  // ...ajoutez toutes les autres images de salades
+};
+
+export const burgerImages = {
+  'Burgers1.jpg': burger1,
+  'Burgers2.jpg': burger2,
+  // ...ajoutez toutes les autres images de burgers
+};
+
+export const pizzaImages = {
+  'Pizzas1.jpg': pizza1,
+  'Pizzas2.jpg': pizza2,
+  // ...ajoutez toutes les autres images de pizzas
+};
+
+export const kebabImages = {
+  'Kebabs1.jpg': kebab1,
+  'Kebabs2.jpg': kebab2,
+  // ...ajoutez toutes les autres images de kebabs
+};
+
+export const dessertImages = {
+  'Desserts1.jpg': dessert1,
+  'Desserts2.jpg': dessert2,
+
+};
+
+
+export const boissonImages = {
+
+  "Boissons1.jpg"  : boisson1,
+  "Boissons2.jpg"  : boisson2,
+  // "Boissons3.jpg"  : boisson3,
+  // "Boissons4.jpg"  : boisson4,
+  // "Boissons5.jpg"  : boisson5,
+  // "Boissons6.jpg"  : boisson6,
+  // "Boissons7.jpg"  : boisson7,
+  // "Boissons8.jpg"  : boisson8,
+  // "Boissons9.jpg"  : boisson9,
+  // "Boissons10.jpg"  : boisson10,
+}
